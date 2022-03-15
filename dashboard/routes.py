186730c667058749251a1b6ee80bdf502f83960a
lawndarts <henryp959@gmail.com>
2022-03-15 04:11:53 -0400

    added stats.py
    Separated functions into a different file

--- dashboard/routes.py
+++ dashboard/routes.py
@@ -4,8 +4,9 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 import praw, requests, json, sys, socket, random
 from dashboard.models import User
-from datetime import datetime
-import numpy as np
+from dashboard import stats
+
+
 
 from dashboard.models import  User
 # from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
@@ -40,75 +41,15 @@ def auth():
 
     return redirect(url_for('dashboard_page'))
 
-
 @app.route('/dashboard')
 def dashboard_page():  
-    jsdict = postingActivityDay()
-    topSubs = topTenSubreddits()
-    something = averageCommentLengths()
+    jsdict = stats.postingActivityDay(reddit)
+    topSubs = stats.topTenSubreddits(reddit)
+    something = stats.averageCommentLengths(reddit)
     return render_template('dashboard.html', jsdict=jsdict, topSubs = topSubs, something = something)
     
 
-def postingActivityDay():
-    supportSubs = ['test', 'videos','pcgaming']
-    DoTW = {'Sunday': 0, 'Monday': 0, 'Tuesday': 0, 'Wednesday': 0, 'Thursday': 0, 'Friday': 0, 'Saturday': 0,}
-    comments =  reddit.user.me().comments.new(limit=50)
-    for comment in comments:
-        if(str(comment.subreddit) in supportSubs):
-            unix_val = datetime.fromtimestamp(comment.created)
-            day = unix_val.weekday()
-            if(day == 0): day = 'Sunday'
-            elif(day == 1): day = 'Monday'
-            elif(day == 2): day = 'Tuesday'
-            elif(day == 3): day = 'Wednesday'
-            elif(day == 4): day = 'Thursday'
-            elif(day == 5): day = 'Friday'
-            elif(day == 6): day = 'Saturday'
-            if(day in DoTW):
-                DoTW[day] += 1
-    return DoTW
 
-
-def topTenSubreddits():
-    subList = {}
-    comments =  reddit.user.me().comments.new(limit=500)
-    for comment in comments:
-        if(str(comment.subreddit) in subList):
-            subList[str(comment.subreddit)] += 1
-        else:
-            subList[str(comment.subreddit)] = 1
-    topSubs = sorted(subList, key=subList.get, reverse=True)[:10]
-    return subList
-#I should probably change this to also compile a list of submissions
-#also this all will probably need to be changed to pull this from a database
-def averageCommentLengths():
-    commentLengths = []
-    supportSubs = ['test', 'videos','pcgaming']
-    wordsMain = {}
-    comments =  reddit.user.me().comments.new(limit=50)
-    for comment in comments:
-        if(str(comment.subreddit) in supportSubs):
-            body = str(comment.body)
-            wordList = body.split()
-            for word in wordList:
-                if word not in wordsMain:
-                    wordsMain[word] = 1
-                else:
-                    wordsMain[word] += 1
-            # if(len(body) > 5): to remove short replies from average
-            commentLengths.append(len(body))
-
-    # print(len(commentLengths))
-    avg = np.average(commentLengths)
-    median = np.median(commentLengths)
-    # print("Average: " , avg)
-    # print("Median: " , median)
-    output = []
-    output.append(avg) 
-    output.append(median)
-    output.append(commentLengths)
-    output.append(wordsMain)
-    return output
 
 
 
